b33361c38c496c2d50a62f0b48dd5b55d1f211b9
zachir <zachir@librem.one>
2023-03-15 19:13:30 -0500

    add script to provide file name for Archive Deck recordings.

---
+++ name.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+CAMPUS="PLD"
+PART_OF_SERVICE="$1"
+ARG="${2:-now}"
+TIME="$(date -d $ARG +"%H%M")"
+DOW="$(date -d $ARG +"%u")"
+DATE="$(date -d $ARG +"%m.%d")"
+if [ "$DOW" -eq 4 -a "$TIME" -gt 1855 -a "$TIME" -lt 2015 ]; then
+  echo "$CAMPUS $PART_OF_SERVICE $DATE - 0700 PM"
+  exit
+elif [ "$DOW" -eq 6 ]; then
+  if [ "$TIME" -gt 1525 -a "$TIME" -lt 1645 ]; then
+    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0330 PM"
+    exit
+  elif [ "$TIME" -gt 1655 -a "$TIME" -lt 1715 ]; then
+    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0500 PM"
+    exit
+  fi
+elif [ "$DOW" -eq 7 ]; then
+  if [ "$TIME" -gt 655 -a "$TIME" -lt 810 ]; then
+    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0700 AM"
+    exit
+  elif [ "$TIME" -gt 810 -a "$TIME" -lt 930 ]; then
+    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0815 AM"
+    exit
+  elif [ "$TIME" -gt 940 -a "$TIME" -lt 1100 ]; then
+    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0945 AM"
+    exit
+  elif [ "$TIME" -gt 1110 -a "$TIME" -lt 1230 ]; then
+    echo "$CAMPUS $PART_OF_SERVICE $DATE - 1115 AM"
+    exit
+  fi
+fi
+date -d $ARG +"$CAMPUS $PART_OF_SERVICE Testing %m.%d - %H%M.mp4"
