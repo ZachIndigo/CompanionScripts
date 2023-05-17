93a02e1a7791b5f150f94727bdd326c4f1942fab
zachir <zachir@librem.one>
2023-05-17 14:39:39 -0500

    Update name.sh to make adding new services easier

--- name.sh
+++ name.sh
@@ -2,33 +2,23 @@
 
 CAMPUS="$1"
 PART_OF_SERVICE="${@:2}"
-TIME="$(date +"%H%M")"
-DOW="$(date +"%u")"
 DATE="$(date +"%m.%d")"
-if [ "$DOW" -eq 4 -a "$TIME" -gt 1855 -a "$TIME" -lt 2015 ]; then
-  echo "$CAMPUS $PART_OF_SERVICE $DATE - 0700 PM"
-  exit
-elif [ "$DOW" -eq 6 ]; then
-  if [ "$TIME" -gt 1525 -a "$TIME" -lt 1645 ]; then
-    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0330 PM"
-    exit
-  elif [ "$TIME" -gt 1655 -a "$TIME" -lt 1715 ]; then
-    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0500 PM"
-    exit
-  fi
-elif [ "$DOW" -eq 7 ]; then
-  if [ "$TIME" -gt 655 -a "$TIME" -lt 810 ]; then
-    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0700 AM"
-    exit
-  elif [ "$TIME" -gt 810 -a "$TIME" -lt 930 ]; then
-    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0815 AM"
-    exit
-  elif [ "$TIME" -gt 940 -a "$TIME" -lt 1100 ]; then
-    echo "$CAMPUS $PART_OF_SERVICE $DATE - 0945 AM"
-    exit
-  elif [ "$TIME" -gt 1110 -a "$TIME" -lt 1230 ]; then
-    echo "$CAMPUS $PART_OF_SERVICE $DATE - 1115 AM"
+DATEFMT="$(date +"%u%H%M")"
+# All of the below are service times
+# (Day of Week).(Time of day in 24hr)
+SERVICES="$(<<EOF
+41855 42015 0700 PM
+70810 70930 0815 AM
+70940 71100 0945 AM
+71110 71230 1115 AM
+EOF
+)"
+
+echo "$SERVICES" | while read i; do
+if [ "$DATEFMT" -ge "$(echo $i | cut -d' ' -f1)" -a "$DATEFMT" -le "$(echo $i | cut -d' ' -f2)" ]; then
+    echo "$CAMPUS $PART_OF_SERVICE $DATE - $(echo "$i" | cut -d' ' -f3,4)"
     exit
   fi
-fi
+done
+
 date +"$CAMPUS $PART_OF_SERVICE Testing %m.%d - %H%M"
