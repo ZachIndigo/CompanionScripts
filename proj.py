f615f8140f5e47e2840c76b927c9b807c77f47a9
Zach Smith <zsmith@newhopechurch.tv>
2025-06-11 17:15:35 -0500

    Split proj into multiple functions

--- proj.py
+++ proj.py
@@ -12,46 +12,74 @@ is_second_part = False
 if args_len <= 2:
     exit()
 
-PROJ_IP_ADDRESS=sys.argv[1]
+PROJ_IP_ADDRESS = sys.argv[1]
 
 pj = Projector(PROJ_IP_ADDRESS)
 
+
+def do_first_part(arg):
+    if arg == "power":
+        return True
+    elif arg == "mute":
+        return True
+    elif arg == "sleep":
+        return True
+    else:
+        return False
+
+
+def do_sleep(arg):
+    try:
+        s = float(arg)
+    except ValueError:
+        print(f"Value {arg} doesn't look like a number!")
+        return False
+    sleep(s)
+    return True
+
+
+def do_power(arg, pj):
+    if arg == "on":
+        pj.set_power(True)
+    elif arg == "off":
+        pj.set_power(False)
+    elif arg == "get":
+        print(pj.get_power())
+    else:
+        print("Command not understood.")
+        return False
+    return True
+
+
+def do_mute(arg, pj):
+    if arg == "on":
+        pj.set_mute(True)
+    elif arg == "off":
+        pj.set_mute(False)
+    elif arg == "get":
+        print(pj.get_mute())
+    else:
+        print("Command not understood.")
+        return False
+    return True
+
+
 for i in range(2, args_len):
-    if is_second_part == False:
-        if sys.argv[i] == "power":
-            is_second_part = True
-        elif sys.argv[i] == "mute":
-            is_second_part = True
-        elif sys.argv[i] == "sleep":
-            is_second_part = True
-        else:
+    if not is_second_part:
+        if not do_first_part(sys.argv[i]):
             print("Command not understood.")
             exit()
+        else:
+            is_second_part = True
         first_part = sys.argv[i]
     else:
         is_second_part = False
         if first_part == "sleep":
-            try:
-                s = float(sys.argv[i])
-            except ValueError:
-                print(f"Value {sys.argv[i]} doesn't look like a number!")
-                continue
-            sleep(s)
+            if not do_sleep(sys.argv[i]):
+                break
         elif first_part == "power":
-            if sys.argv[i] == "on":
-                pj.set_power(True)
-            elif sys.argv[i] == "off":
-                pj.set_power(False)
-            elif sys.argv[i] == "get":
-                print(pj.get_power())
-            else:
-                print("Command not understood.")
+            if not do_power(sys.argv[i], pj):
+                break
         elif first_part == "mute":
-            if sys.argv[i] == "on":
-                pj.set_mute(True)
-            elif sys.argv[i] == "off":
-                pj.set_mute(False)
-            elif sys.argv[i] == "get":
-                print(pj.get_mute())
-            else:
-                print("Command not understood.")
+            if not do_mute(sys.argv[i], pj):
+                break
